5191a369b4f35e20d877d64ff415fe3eaea3aa17
IcosahedralDice <etomnz@gmail.com>
2020-07-22 16:38:02 +1200

    Initialisation script

---
+++ init.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+types_q = '''CREATE TABLE IF NOT EXISTS event_types (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        type TEXT
+                                    ); '''
+event_q = '''CREATE TABLE IF NOT EXISTS events (
+                                    id integer PRIMARY KEY,
+                                    type_id integer, 
+                                    time integer, 
+                                    data text, 
+                                    notes text, 
+                                    FOREIGN KEY (type_id) REFERENCES event_types (id)
+                                );'''
+db = sqlite3.connect('data/test.db')
+cursor = db.cursor()
+cursor.execute(types_q)
+cursor.execute(event_q)
+print('Data tables created. ')
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p data/
+python3.8 init.py
