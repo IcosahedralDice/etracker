a2b3de43f033cdf279f9cfd9af8c63d18ee43201
IcosahedralDice <etomnz@gmail.com>
2020-07-25 10:32:17 +1200

    Have dynamic # rows again

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ max_rows = 1000
 
 
 def reload_page(table: QtWidgets.QTableWidget, data):
+    table.setUpdatesEnabled(False)
+    table.setRowCount(min(max_rows, len(data)))
     for i in range(min(max_rows, len(data))):
         for j in range(len(data[i])):
-            table.item(i, j).setText(data[i][j])
-    for i in range(min(max_rows, len(data)), max_rows):
-        for j in range(4):
-            table.item(i, j).setText('')
+            table.setItem(i, j, QTableWidgetItem(data[i][j]))
     table.resizeColumnsToContents()
     table.resizeRowsToContents()
     table.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+    table.setUpdatesEnabled(True)
+
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -25,12 +26,12 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         # Load the UI Page
-        uic.loadUi('MainWindow.ui', self)
+        uic.loadUi('uifiles/MainWindow.ui', self)
         header = self.tableWidget.horizontalHeader()
         header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
 
+        self.tableWidget.setUpdatesEnabled(False)
         self.tableWidget.setRowCount(max_rows)
-        ohead = self.tableWidget.verticalHeader()
         for i in range(max_rows):
             for j in range(4):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(''))
@@ -38,7 +39,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.searchBar.textEdited.connect(
             lambda x: reload_page(self.tableWidget, backend.retrieve_events(x, limit=max_rows)))
 
-        reload_page(self.tableWidget, backend.retrieve_events())
+        reload_page(self.tableWidget, backend.retrieve_events(limit=max_rows))
 
 
 app = QtWidgets.QApplication(sys.argv)
